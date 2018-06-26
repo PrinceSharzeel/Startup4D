66463a4a7c9d042e89629c565cf5754979a4d20e
dPSudocoder <princesharzeel.10@gmail.com>
2018-06-26 07:11:17 +0530

    Update settings.py

--- startup4d/settings.py
+++ startup4d/settings.py
@@ -84,12 +84,8 @@ WSGI_APPLICATION = 'startup4d.wsgi.application'
 
 DATABASES = {
     'default': {
-         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dbname',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '',
-        'PORT': '',
+       'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
